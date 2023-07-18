fb03eca8e873b6d2ac80475dbc16da200e4cf5b2
Tantannn <tango.11@yahoo.com>
2023-07-19 00:26:56 +0700

    first commit

--- src/Component/Nav/Nav.tsx
+++ src/Component/Nav/Nav.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./Nav.css";
+import { VscGrabber, VscClose } from "react-icons/vsc";
 const Nav = () => {
-
+  const [isActive, setisActive] = useState(false);
+  const handleToggle = () => {
+    setisActive(!isActive);
+  }
   return (
     <nav id="navbar" className="nav">
       <ul className="nav-list">
@@ -12,6 +17,11 @@ const Nav = () => {
         </li>
         <li>
           <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <button className="menu__button  nav_ac" onClick={handleToggle}>
+            {!isActive ? <VscClose /> : <VscGrabber />}
+          </button>
         </li>
       </ul>
     </nav>
