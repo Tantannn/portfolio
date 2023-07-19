a77e5fe7cba4a68996a3c7fa125f2946119641dc
Tantannn <tango.11@yahoo.com>
2023-07-19 16:09:20 +0700

    update

--- src/Component/Nav/Nav.tsx
+++ src/Component/Nav/Nav.tsx
@@ -1,30 +1,34 @@
 import { useState } from "react";
 import "./Nav.css";
 import { VscGrabber, VscClose } from "react-icons/vsc";
+import NavDropdown from "./NavDropdown";
 const Nav = () => {
   const [isActive, setisActive] = useState(false);
   const handleToggle = () => {
     setisActive(!isActive);
   }
   return (
-    <nav id="navbar" className="nav">
-      <ul className="nav-list">
-        <li>
-          <a href="#welcome-section">About</a>
-        </li>
-        <li>
-          <a href="#projects">Work</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-        <li>
-          <button className="menu__button  nav_ac" onClick={handleToggle}>
-            {!isActive ? <VscClose /> : <VscGrabber />}
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav id="navbar" className="nav">
+        <ul className="nav-list">
+          <li>
+            <a href="#welcome-section">About</a>
+          </li>
+          <li>
+            <a href="#projects">Work</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <button className="" onClick={handleToggle}>
+              {!isActive ? <VscClose /> : <VscGrabber />}
+            </button>
+          </li>
+        </ul>
+      </nav>
+      { isActive ? <NavDropdown/> : null}
+    </div>
   );
 };
 
---
+++ src/Component/Nav/NavDropdown.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NavDropdown() {
+  return (
+    <div className="nav-dropdown">
+      <p>Hello</p>
+    </div>
+  )
+}
